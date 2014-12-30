68bce80e463d35debd4588e9381a8efcb07bed8b
Ryan Saeta <saetar@carleton.edu>
2014-12-30 14:32:08 -0800

    Evaluator works!
    I’ve changed god to a queue, rather than a stack which is what I now
    realize I want for evaluation. Fixed part of tokenizer that would
    remove ALL instances of certain substrings, but now doesn’t do that. :)
    
    Fixed the issue of leaving the operator stack empty.
    
    More fixes to come:
    This is not yet an integrator. I’m playing around with different
    numerical methods of integration rather than just brute force Reimann
    Sum.
    There are definitely some places that could be made more efficient.
    Also need to make more comments.

--- Integrator/src/Integrator.java
+++ Integrator/src/Integrator.java
@@ -11,7 +11,7 @@ public class Integrator{
 	public static ArrayList<Pattern> patterns;
 	public static ArrayList<String> tokens;
 	public static Stack<String> opStack;
-	public static Stack<String> god;
+	public static Queue<String> god;
 
 	public static void main(String[] args){
 		//Finds the String representation of the function, and the variable with respect to which it is integrated.
@@ -43,9 +43,10 @@ public class Integrator{
 		print(tokens);
 		
 		opStack = new Stack<String>();
-		god = new Stack<String>();
+		god = new ArrayDeque<String>();
 		makeGod();
 		print(god);
+		System.out.println(evaluate(2));
 		//System.out.println(integrate(0,2));
 		scan.close();
 	}
@@ -63,7 +64,7 @@ public class Integrator{
 				interest = matcher.group();
 				System.out.println("interest: " + interest);
 				tokens.add(interest);
-				str=str.replace(interest, "");
+				str=str.substring(interest.length());
 				break;
 			}
 		}
@@ -100,13 +101,14 @@ public class Integrator{
 	}
 	
 	public static void makeGod(){
+		
 		for(int i = 0; i < tokens.size(); i++){
 			String s = tokens.get(i);
 			char c = s.charAt(0);
 			
 			//If c is part of a number, add it to the queue
 			if(Character.isDigit(c)||c==variable){
-				god.push(tokens.get(i));
+				god.add(tokens.get(i));
 			}
 			
 			//If c is addition or subtraction
@@ -116,7 +118,7 @@ public class Integrator{
 					//If the other things had a higher precedent, then pop operators onto queue
 					if(opStack.peek().charAt(0)!='+' && opStack.peek().charAt(0)!='-'){
 						while(opStack.peek().charAt(0) != '(' && opStack.peek().charAt(0)!='+' && opStack.peek().charAt(0)!='-' && !opStack.empty()){
-							god.push(opStack.pop());
+							god.add(opStack.pop());
 						}
 					}
 					//push it onto the stack
@@ -132,7 +134,7 @@ public class Integrator{
 					//If the other things had a higher precedent, then pop operators onto queue
 					if(t!='/'&&t!='*' && t!='+' && t!='-' && t!='('){
 						while(t!='/'&&t!='*' && t!='+' && t!='-' && t!='('){
-							god.push(opStack.pop());
+							god.add(opStack.pop());
 						}
 					}
 					//If same/lower priority, push it onto opStack
@@ -142,9 +144,24 @@ public class Integrator{
 				}
 			}
 			
-			//If it's an exponent, nothing has higher priority, so always push
+			//If it's an exponent, only sin, log, etc. will have higher precedence
 			else if(c=='^'){
-				opStack.push(s);
+				if(opStack.empty()) opStack.push(s);
+				else{
+					char t = opStack.peek().charAt(0);
+					//If the other things had a higher precedent, then pop operators onto queue
+					if(t!='*' && t!='/'&& t!='s' && t!='c' && t!='t' && t!='l' && t!='('){
+						while(t!='/' && t!='*' && t!='s'&&t!='c' && t!='t' && t!='l' && t!='('){
+							god.add(opStack.pop());
+							t = opStack.peek().charAt(0);
+						}
+						opStack.push(s);
+					}
+					//If same/lower priority, push it onto opStack
+					else{
+						opStack.push(s);
+					}
+				}
 			}
 			
 			//If it's a pre-defined function like sin and log, then
@@ -162,24 +179,91 @@ public class Integrator{
 			//god stack, until reaching opening parentheses
 			else if(c == ')'){
 				while(opStack.peek().charAt(0)!='('){
-					god.push(opStack.pop());
+					god.add(opStack.pop());
 				}
 				//Remove the opening paren from opStack
 				//WE DON'T NEED ANY PAREN
 				opStack.pop();
 			}
 		}
+		print(opStack);
 		//Pop all leftover operators onto god
-		for(int i = 0; i < opStack.size(); i++){
-			god.push(opStack.pop());
+		while(!opStack.empty()){
+			god.add(opStack.pop());
 		}
 	}
 
-	//evaluates God at x
+	//evaluates god at x
 	public static double evaluate(double x){
+		Queue<String> temp = god;
+		Stack<String> eval = new Stack<String>();
+		String a;
+		double z;
+		double y;
 		
-		
-		return x;
+		while(!temp.isEmpty()){
+			String toke = temp.poll();
+			char t = toke.charAt(0);
+
+			if(Character.isDigit(t)){
+				eval.push(toke);
+			}
+			
+			else if(t == variable){
+				eval.push(Double.toString(x));
+			}
+			
+			if(t=='s'){
+				a = eval.pop();
+				eval.push(String.valueOf(Math.sin(Double.parseDouble(a))));
+			}
+			
+			if(t=='c'){
+				a = eval.pop();
+				eval.push(String.valueOf(Math.cos(Double.parseDouble(a))));
+			}
+			
+			if(t=='t'){
+				a = eval.pop();
+				eval.push(String.valueOf(Math.tan(Double.parseDouble(a))));
+			}
+			
+			if(t=='l'){
+				a = eval.pop();
+				eval.push(String.valueOf(Math.log(Double.parseDouble(a))));
+			}
+	
+			if(t=='+'){
+				z = Double.parseDouble(eval.pop());
+				y = Double.parseDouble(eval.pop());
+				eval.add(String.valueOf(z+y));
+			}
+			
+			if(t=='-'){
+				z = Double.parseDouble(eval.pop());
+				y = Double.parseDouble(eval.pop());
+				eval.add(String.valueOf(y-z));
+			}
+			
+			if(t=='*'){
+				z = Double.parseDouble(eval.pop());
+				y = Double.parseDouble(eval.pop());
+				eval.add(String.valueOf(z*y));
+			}
+			
+			if(t=='/'){
+				z = Double.parseDouble(eval.pop());
+				y = Double.parseDouble(eval.pop());
+				eval.add(String.valueOf(y/z));
+			}
+			
+			if(t=='^'){
+				z = Double.parseDouble(eval.pop());
+				y = Double.parseDouble(eval.pop());
+				eval.add(String.valueOf(Math.pow(z, y)));
+			}
+		}
+		return Double.parseDouble(eval.pop());
 	}
 	
 	public static double integrate(double start, double end){
@@ -192,7 +276,7 @@ public class Integrator{
 		return answer;
 	}
 	
-	public static void print(Collection c){
+	public static void print(Collection<String> c){
 		System.out.println(c);
 	}
 }
