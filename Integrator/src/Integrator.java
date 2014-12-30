56ffe041751049b24bf16096ae474f40de470e59
Ryan Saeta <saetar@carleton.edu>
2014-12-29 22:45:31 -0800

    TOKENIZE!
    Changed course again, away from tree structure, and onto a tokenizer.
    This breaks up the expression nicely into individual components of an
    expression. I’ve also written a function to change the raw tokens into
    RPN which will hopefully be easier to evaluate. (Key word: hopefully.
    Still haven’t written evaluate)
    
    P.S. More comments were made!

--- Integrator/src/Integrator.java
+++ Integrator/src/Integrator.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
 	Has main function
@@ -6,8 +8,10 @@ import java.util.*;
 public class Integrator{
 
 	public static char variable;
-	public static Tree god;
-	//public static Operator god;
+	public static ArrayList<Pattern> patterns;
+	public static ArrayList<String> tokens;
+	public static Stack<String> opStack;
+	public static Stack<String> god;
 
 	public static void main(String[] args){
 		//Finds the String representation of the function, and the variable with respect to which it is integrated.
@@ -16,20 +20,179 @@ public class Integrator{
 		System.out.println("What expression would you like to integrate?");
 		String expression = scan.nextLine();
 		System.out.println("And with respect to what variable?");
+		patterns = new ArrayList<Pattern>();
 		variable = scan.nextLine().charAt(0);
-		god = new Tree(expression);
-		System.out.println(integrate(0,2));
+		tokens = new ArrayList<String>();
+		
+		//A token is either a number (string of digits)
+		Pattern number = Pattern.compile("^(\\d+)");
+		//or a token is one character (parentheses or binary operator)
+		Pattern onechar = Pattern.compile("^(\\*|\\/|\\+|\\-|\\^|\\(|\\)|"+variable+ ")");
+		//or a token is a trig/log function etc.
+		Pattern complex = Pattern.compile("^sin|cos|tan|log|ln");
+
+		
+		patterns.add(number);
+		patterns.add(onechar);
+		patterns.add(complex);
+
+
+		tokenize(expression);
+		print(tokens);
+		preprocess();
+		print(tokens);
+		
+		opStack = new Stack<String>();
+		god = new Stack<String>();
+		makeGod();
+		print(god);
+		//System.out.println(integrate(0,2));
 		scan.close();
 	}
+	
+	//Takes in a string and tokenizes it based on patterns
+	public static void tokenize(String str){
+		if(str.equals(""))
+			return;
+		String interest="";
+		//Search for pattern match
+		for(int i = 0; i < patterns.size(); i++){
+			Matcher matcher = patterns.get(i).matcher(str);
+			if(matcher.find()){
+				System.out.println("Found " + i + "th matcher ");
+				interest = matcher.group();
+				System.out.println("interest: " + interest);
+				tokens.add(interest);
+				str=str.replace(interest, "");
+				break;
+			}
+		}
+		System.out.println(str);
+		tokenize(str);
+	}
+	
+	/* Preprocess takes care of implicit multiplication
+	 * e.g. 4sin(x) = 4*sin(x) and 4x = 4*x
+	 * The rule here is if there is a number token
+	 * followed by anything but a binary operator token,
+	 * then add "*" token between them.
+	 * Could also be variable followed by not a binary operator
+	 * like xsin(x) in which case it would need a * as well.
+	 * HOWEVER, closing paren do not need a *
+	 */
+	public static void preprocess(){
+		String star = "*";
+		for(int i = 0; i < tokens.size()-1; i++){
+			
+			if(tokens.get(i).matches("\\d*")){
+				if(!tokens.get(i+1).matches("^\\*|\\/|\\+|\\-|\\^|\\)")){
+					tokens.add(i+1, star);
+				}
+			}
+			
+			if(tokens.get(i).matches(variable+"")){
+				if(!tokens.get(i+1).matches("^\\*|\\/|\\+|\\-|\\^")){
+					tokens.add(i+1, star);
+				}
+			}
+			
+		}
+	}
+	
+	public static void makeGod(){
+		for(int i = 0; i < tokens.size(); i++){
+			String s = tokens.get(i);
+			char c = s.charAt(0);
+			
+			//If c is part of a number, add it to the queue
+			if(Character.isDigit(c)||c==variable){
+				god.push(tokens.get(i));
+			}
+			
+			//If c is addition or subtraction
+			else if(c=='+'|| c=='-'){
+				if(opStack.empty()) opStack.push(s);
+				else{
+					//If the other things had a higher precedent, then pop operators onto queue
+					if(opStack.peek().charAt(0)!='+' && opStack.peek().charAt(0)!='-'){
+						while(opStack.peek().charAt(0) != '(' && opStack.peek().charAt(0)!='+' && opStack.peek().charAt(0)!='-' && !opStack.empty()){
+							god.push(opStack.pop());
+						}
+					}
+					//push it onto the stack
+						opStack.push(s);
+				}
+			}
+			
+			//If multiplication or division
+			else if(c=='*' || c=='/'){
+				if(opStack.empty()) opStack.push(s);
+				else{
+					char t = opStack.peek().charAt(0);
+					//If the other things had a higher precedent, then pop operators onto queue
+					if(t!='/'&&t!='*' && t!='+' && t!='-' && t!='('){
+						while(t!='/'&&t!='*' && t!='+' && t!='-' && t!='('){
+							god.push(opStack.pop());
+						}
+					}
+					//If same/lower priority, push it onto opStack
+					else{
+						opStack.push(s);
+					}
+				}
+			}
+			
+			//If it's an exponent, nothing has higher priority, so always push
+			else if(c=='^'){
+				opStack.push(s);
+			}
+			
+			//If it's a pre-defined function like sin and log, then
+			//it has a higher priority to everything
+			else if(c=='s' || c=='c' || c=='t' || c=='l'){
+				opStack.push(s);
+			}
+			
+			//If it is an opening parentheses, then push it onto opstack
+			else if(c=='('){
+				opStack.push(s);
+			}
+			
+			//If it is a closing parentheses, then pop all operators onto 
+			//god stack, until reaching opening parentheses
+			else if(c == ')'){
+				while(opStack.peek().charAt(0)!='('){
+					god.push(opStack.pop());
+				}
+				//Remove the opening paren from opStack
+				//WE DON'T NEED ANY PAREN
+				opStack.pop();
+			}
+		}
+		//Pop all leftover operators onto god
+		for(int i = 0; i < opStack.size(); i++){
+			god.push(opStack.pop());
+		}
+	}
 
+	//evaluates God at x
+	public static double evaluate(double x){
+		
+		
+		return x;
+	}
 	
 	public static double integrate(double start, double end){
 		double answer = 0;
-		for(double i = start; i < end; i+=.00001){
+		/*for(double i = start; i < end; i+=.00001){
 			answer += god.traverse(i);
 		}
 		answer /= 100000;
-		System.out.println(answer);
+		System.out.println(answer);*/
 		return answer;
+	}
+	
+	public static void print(Collection c){
+		System.out.println(c);
 	}
 }
--- src/Tree.java
+++ src/Tree.java
@@ -11,31 +11,32 @@ public class Tree {
 	
 	public Tree(String str) {
 		variable = 'x';
-		
+		System.out.println("Making tree with: " + str);
 		//Establish patterns of possible functions
 		patterns = new ArrayList<Pattern>();
 		//Negative lookahead on trig/log functions and operators at the end (adds a * at end)
+		Pattern log = Pattern.compile("\\d*(log|ln)(.*)");
+		Pattern sin = Pattern.compile("(.*)sin(.*)");
+		Pattern cos = Pattern.compile("(.*)cos(.*)");
+		Pattern tan = Pattern.compile("(.*)tan(.*)");
 		Pattern paren1 = Pattern.compile("(?!sin|cos|tan|log|ln)(.*)\\((.*)\\)");
 		Pattern paren2 = Pattern.compile("(?!sin|cos|tan|log|ln)(.*)\\((.*)\\)(\\-\\+\\/)?(.*)");
 		Pattern expon = Pattern.compile("(.*)\\^(.*)");
 		Pattern md = Pattern.compile("(.*)(\\*|\\/)(.*)");
-		Pattern log = Pattern.compile("\\d*(log|ln)(.*)");
-		Pattern sin = Pattern.compile("(.*)(sin)(.*)");
-		Pattern cos = Pattern.compile("(.*)(cos)(.*)");
-		Pattern tan = Pattern.compile("(.*)(tan)(.*)");
 		Pattern linear = Pattern.compile("(\\d*)"+variable);
 		Pattern as = Pattern.compile("(.*)(\\+|\\-)(.*)");
 		Pattern constant = Pattern.compile("^\\d*$");
 		//Try making sin/cos/tan... operators
-		patterns.add(paren1);
-		patterns.add(paren2);
-		patterns.add(expon);
-		patterns.add(md);
+		
 		patterns.add(log);
 		patterns.add(sin);
 		patterns.add(cos);
 		patterns.add(tan);
+		patterns.add(paren1);
+		patterns.add(paren2);
 		patterns.add(as);
+		patterns.add(md);
+		patterns.add(expon);
 		patterns.add(linear);
 		patterns.add(constant);
 		root = insert(str);
@@ -56,43 +57,8 @@ public class Tree {
 				String str = matcher.group();
 				switch(pcount){
 					//recursive call to evaluate before and inside paren 
-					case 0:
-						temp = new Node('*');
-						temp.setLeft(insert(matcher.group(2)));
-						String str2 = str.replace(matcher.group(2),"");
-						temp.setRight(insert(str2));
-						return temp;
-	
-					//recursive call to evaluate inside paren and after
-					case 1:
-						//If has group 4, then it has that op, otherwise it's *
-						if(matcher.group(4).equals("")){
-							temp = new Node('*');
-						}
-						else{
-							temp = new Node(matcher.group(4).charAt(0));
-						}
-						temp.setLeft(insert(matcher.group(3)));
-						String str3 = matcher.group(5);
-						temp.setRight(insert(str3));
-						return temp;
-	
-					//Create an exponent
-					case 2:
-						temp = new Node('^');
-						temp.setLeft(insert(matcher.group(1)));
-						temp.setRight(insert(matcher.group(2)));
-						return temp;
-						
-					//Multiplication or Division	
-					case 3:
-						temp = new Node(matcher.group(2).charAt(0));
-						temp.setLeft(insert(matcher.group(1)));
-						temp.setRight(insert(matcher.group(3)));
-						return temp;
-						
 					//Log	
-					case 4:
+					case 0:
 						if(matcher.group(1).equals("")){
 							f = new Log(str, variable);
 							temp = new Node(f);
@@ -106,9 +72,11 @@ public class Tree {
 						return temp;
 					
 					//Sin
-					case 5:
+					case 1:
+						String strfoo = matcher.group(2);
+						System.out.println("Found sin");
 						if(matcher.group(1).equals("")){
-							f = new Sin(str, variable);
+							f = new Sin(strfoo, variable);
 							temp = new Node(f);
 						}
 						else{
@@ -120,7 +88,7 @@ public class Tree {
 						return temp;
 				
 					//Cos	
-					case 6:
+					case 2:
 						if(matcher.group(1).equals("")){
 							f = new Cos(str, variable);
 							temp = new Node(f);
@@ -134,7 +102,7 @@ public class Tree {
 						return temp;
 					
 					//Tan
-					case 7:
+					case 3:
 						if(matcher.group(1).equals("")){
 							f = new Tan(str, variable);
 							temp = new Node(f);
@@ -146,14 +114,53 @@ public class Tree {
 							temp.setRight(insert(foo));
 						}
 						return temp;
-					
+						
+					case 4:
+						System.out.println(str);
+						temp = new Node('*');
+						temp.setLeft(insert(matcher.group(1)));
+						String str2 = str.replace(matcher.group(1),"");
+						if(str2.equals(str)){
+							str2 = matcher.group(2);
+						}
+						temp.setRight(insert(str2));
+						return temp;
+	
+					//recursive call to evaluate inside paren and after
+					case 5:
+						//If has group 4, then it has that op, otherwise it's *
+						if(matcher.group(4).equals("")){
+							temp = new Node('*');
+						}
+						else{
+							temp = new Node(matcher.group(4).charAt(0));
+						}
+						temp.setLeft(insert(matcher.group(3)));
+						String str3 = matcher.group(5);
+						temp.setRight(insert(str3));
+						return temp;
+
 					//Addition/Subtraction
-					case 8:
+					case 6:
 						temp = new Node(matcher.group(2).charAt(0));
 						temp.setLeft(insert(matcher.group(1)));
 						temp.setRight(insert(matcher.group(3)));
 						return temp;
 						
+					//Multiplication or Division	
+					case 7:
+						temp = new Node(matcher.group(2).charAt(0));
+						temp.setLeft(insert(matcher.group(1)));
+						temp.setRight(insert(matcher.group(3)));
+						return temp;
+												
+					//Create an exponent
+					case 8:
+						temp = new Node('^');
+						temp.setLeft(insert(matcher.group(1)));
+						temp.setRight(insert(matcher.group(2)));
+						return temp;
+
 					//Linear	
 					case 9:
 						f = new Linear(str, variable);
